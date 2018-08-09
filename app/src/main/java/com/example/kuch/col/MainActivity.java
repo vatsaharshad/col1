1442277aa6df699a6b17b5066fc46002a4f9321b
Harshad <vatsa406@gmail.com>
2018-08-09 23:19:14 +0530

    1st Draft, Splash Screen

--- app/src/main/java/com/example/kuch/col/MainActivity.java
+++ app/src/main/java/com/example/kuch/col/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.kuch.col;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         Spinner mySpinner = (Spinner) findViewById(R.id.spinner1);
 
